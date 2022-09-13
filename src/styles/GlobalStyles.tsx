c4b05df6fe9520b93051fef62e6975042bd03fd8
jameschain <james.cordova423@gmail.com>
2022-09-13 04:10:21 -0400

    init project

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { GlobalStyles as BaseStyles } from 'twin.macro';
+const CustomStyles = createGlobalStyle`
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button {
+      -webkit-appearance: none; 
+      margin: 0; 
+    }
+`;
+
+const GlobalStyles: FC = () => (
+  <>
+    <BaseStyles />
+    <CustomStyles />
+  </>
+);
+
+export default GlobalStyles;
